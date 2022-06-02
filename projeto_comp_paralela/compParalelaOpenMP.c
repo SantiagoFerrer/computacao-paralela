d3ff2f5763f1b1af5b6aa621acedc962f068fae0
Lucas02-py <78734407+Lucas02-py@users.noreply.github.com>
2022-06-02 14:56:48 -0300

    Update compParalelaOpenMP.c

--- projeto_comp_paralela/compParalelaOpenMP.c
+++ projeto_comp_paralela/compParalelaOpenMP.c
@@ -2,27 +2,28 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define thread_count 4
+#define thread_count 2
 
 double S = 0;
 
-void Taylor(int t)
+void Taylor(double t)
 {
 	double aux;
-	aux = 1/t;
-  #pragma omp critical
-    S += aux;
+	aux = 1 / t;
+#pragma omp critical
+	S += aux;
 }
 
 int main()
 {
-   S = 0;
-  #pragma omp parallel for num_threads(thread_count)
-	for (int t = 0; t <= 1000; t++)
-  {
-    Taylor (t);
+	S = 0;
+#pragma omp parallel for num_threads(thread_count)
+	for (int t = 1; t <= 1000000; t++)
+	{
+		double n = t;
+		Taylor(n);
 	}
-  printf("S = %f",S);
+	printf("S = %f", S);
 	return 0;
-   
+
 }
