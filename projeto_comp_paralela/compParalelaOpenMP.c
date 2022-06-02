a5ce4815291d9f62302356e127280dfba01e7505
Lucas02-py <78734407+Lucas02-py@users.noreply.github.com>
2022-06-02 16:50:47 -0300

    Update compParalelaOpenMP.c

--- projeto_comp_paralela/compParalelaOpenMP.c
+++ projeto_comp_paralela/compParalelaOpenMP.c
@@ -4,20 +4,15 @@
 
 #define thread_count 6
 
-double S = 0;
-
 int main()
 {
-    S = 0;
-    double aux;
-#pragma omp parallel for num_threads(thread_count) reduction(+:aux)
-    for (int t = 1; t <= 100; t++)
-    {
-        double n = t;
-        aux += 1 / n;
+    double aux;                                                     //variável local que armazena os resultados das threads
+#pragma omp parallel for num_threads(thread_count) reduction(+:aux) // "parallel for" cria uma thread a cada iteração
+    for (int t = 1; t <= 100; t++)                                  // "num_thread" define quantas threads podem ser criadas por vez
+    {                                                               // no caso, 6.
+        double n = t;                                               // "reduction(+:aux)" define que os resultados de cada thread serão
+        aux += 1 / n;                                               // unidos na variável aux.
     }
-    S = aux;
-    printf("S = %f", S);
+    printf("S = %f", aux);
     return 0;
-
 }
